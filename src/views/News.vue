8e36d438328da5afea43c8764a6cded3809ac7d4
PonPechraksmey0 <139233349+PonPechraksmey@users.noreply.github.com>
2024-07-06 23:28:29 +0700

    create page New

--- src/views/News.vue
+++ src/views/News.vue
@@ -3,9 +3,100 @@
 </script>
 
 <template>
-    <h1>News page</h1>
+    <div>
+        <h1 class="text-center pt-12">ព័ត៏មានថ្មីៗ</h1>
+    </div>
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex w-5/6 h-80 mt-8 border-2 border-gray-300">
+            <div class="w-1/3 h-78">
+                <img src="/public/images/content/Rectangle 43.png" alt="" style="width: 100%; height: 100%; object-fit: cover;">
+            </div>
+            <div class="flex flex-col w-2/3 h-78">
+                <div class="w-full h-[250px]">
+                    <p class="p-8">
+                        ដំណើរទស្សនកិច្ចសិក្សាទៅកាន់ខេត្តមណ្ឌលគិរី ក្នុងដំណើរការ ទស្សនកិច្ចនេះនិសិ្សតបានចុះកិច្ចសិក្សានិងប្រមូលព័ត៍មានដំណើរ
+                        ក្នុងដំណើរការ ទស្សនកិច្ចនេះ និសិ្សតបានចុះកិច្ចសិក្សា និងប្រមូលព័ត៍មាន...
+                    </p>
+                </div>
+                <div class="flex justify-between items-center w-full h-[67px] p-8">
+                    <p class="text-left flex-grow" style="font-size: 20px;">(ភ្នំពេញ)៖ នាព្រឹកថ្ងៃអង្គារ ទី២ ខែកក្តដា ឆ្នាំ២០២៤</p>
+                    <button class="w-[100px] h-[50px] rounded-lg">អានបន្ថែម</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex w-5/6 h-80 mt-8 border-2 border-gray-300">
+            <div class="w-1/3 h-78">
+                <img src="/public/images/content/Rectangle 44.png" alt="" style="width: 100%; height: 100%; object-fit: cover;">
+            </div>
+            <div class="flex flex-col w-2/3 h-78">
+                <div class="w-full h-[250px]">
+                    <p class="p-8">
+                        ការកំណត់សមាសភាពក្រុមប្រឹកក្សាភិបាលសិក្សាទីផ្សារ និងផ្សាយទេសចរណ៍កម្ពុជា (ភ្នំពេញ)៖ រាជរដ្ឋាភិបាលបានចេញសេចក្ដីសម្រេច...
+                    </p>
+                </div>
+                <div class="flex justify-between items-center w-full h-[67px] p-8">
+                    <p class="text-left flex-grow" style="font-size: 20px;">(ភ្នំពេញ)៖ នាព្រឹកថ្ងៃអង្គារ ទី២ ខែកក្តដា ឆ្នាំ២០២៤</p>
+                    <button class="w-[100px] h-[50px] rounded-lg">អានបន្ថែម</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex w-5/6 h-80 mt-8 border-2 border-gray-300">
+            <div class="w-1/3 h-78">
+                <img src="/public/images/content/Rectangle 45.png" alt="" style="width: 100%; height: 100%; object-fit: cover;">
+            </div>
+            <div class="flex flex-col w-2/3 h-78">
+                <div class="w-full h-[250px]">
+                    <p class="p-8">
+                        សហគ្រិនខ្មែរ, សមាគមអ្នកផលិត ចំណីអាហារកម្អុជា, សមាគម សហគ្រិនស្រ្ដីកម្ពុជា, សមាគមសហគ្រិន ស្រ្ដីកម្ពុជាខេត្តកោះកុង និងអង្គការ...
+                    </p>
+                </div>
+                <div class="flex justify-between items-center w-full h-[67px] p-8">
+                    <p class="text-left flex-grow" style="font-size: 20px;">(ភ្នំពេញ)៖ នាព្រឹកថ្ងៃអង្គារ ទី២ ខែកក្តដា ឆ្នាំ២០២៤</p>
+                    <button class="w-[100px] h-[50px] rounded-lg">អានបន្ថែម</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex w-5/6 h-80 mt-8 border-2 border-gray-300">
+            <div class="w-1/3 h-78">
+                <img src="/public/images/content/Rectangle 46.png" alt="" style="width: 100%; height: 100%; object-fit: cover;">
+            </div>
+            <div class="flex flex-col w-2/3 h-78">
+                <div class="w-full h-[250px]">
+                    <p class="p-8">
+                        រដ្ឋមន្រ្ដីក្រសួងអប់រំ កោតសរសើរ និងវាយតម្លៃខ្ពស់លើកម្មវិញសិក្សា ដ៏សម្បូរបែប របស់សាលាអន្ដរជាតិ​ ស៊ី អាយ អេ ហ្វឺសត៍ ជាពិសេសកម្ម...
+                    </p>
+                </div>
+                <div class="flex justify-between items-center w-full h-[67px] p-8">
+                    <p class="text-left flex-grow" style="font-size: 20px;">(ភ្នំពេញ)៖ នាព្រឹកថ្ងៃអង្គារ ទី២ ខែកក្តដា ឆ្នាំ២០២៤</p>
+                    <button class="w-[100px] h-[50px] rounded-lg">អានបន្ថែម</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style>
-
+<style scoped>
+   h1{
+          font-family: 'Khmer OS Muol';
+          font-style: normal;
+          font-weight: 300;
+          font-size: xx-large;
+      }
+      p{
+          font-family: 'Khmer OS Battambang';
+          font-style: normal;
+          font-weight: normal;
+          font-size: xx-large;
+      }
+      button{
+        font-family: Khmer OS Battambang;
+        background-color: rgb(98, 149, 162);
+      }
 </style>
+
